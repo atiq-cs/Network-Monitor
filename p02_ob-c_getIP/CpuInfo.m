582f4fc3681962a5b09fc46a97b181f95b646030
matrix01 <milon06@gmail.com>
2015-12-23 23:55:51 +0600

    File create and write

--- p02_ob-c_getIP/CpuInfo.m
+++ p02_ob-c_getIP/CpuInfo.m
@@ -12,44 +12,20 @@
 
 typedef struct kinfo_proc kinfo_proc;
 - (void)writeToFile:(NSString*)string {
-    NSFileHandle *file;
-    //object for File Handle
-    NSError *error;
-    //crearing error object for string with file contents format
-    NSMutableData *writingdatatofile;
     
-    //file read and write start
-    NSString *filePath=[NSString stringWithFormat:@"/Users/milan/Desktop/text.txt"];
-    //telling about File Path for Reading for easy of access
-    file = [NSFileHandle fileHandleForReadingAtPath:@"/Users/milan/Desktop/text.txt"];
-    //assign file path directory
-    if (file == nil) //check file exist or not
-        NSLog(@"Failed to open file");
-    NSString *getfileContents = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:&error];
-    //access file contents with out ns handle method
-    if (error) //check error flag for file present or not
-        NSLog(@"Error reading file: %@", error.localizedDescription);
-    //NSLog(@"contents: %@", getfileContents);
-    
-    //display file contents in main file
-    //NSArray *listArray = [getfileContents componentsSeparatedByString:@"\n"];
-    //caluculate list of line present in files
-    //NSLog(@"items = %ld", [listArray count]);
-    const char *writingchar = [string UTF8String];
-    writingdatatofile = [NSMutableData dataWithBytes:writingchar length:strlen(writingchar)];
-    //convert string format into ns mutable data format
-    file = [NSFileHandle fileHandleForUpdatingAtPath: @"/Users/milan/Desktop/text.txt"];
-    //set writing path to file
-    if (file == nil) //check file present or not in file
-        NSLog(@"Failed to open file");
-    [file seekToFileOffset: [getfileContents length]];
-    //object pointer initialy points the offset as 6 position in file
-    [file writeData: writingdatatofile];
-    //writing data to new file
-    [file closeFile];
-    //close the file
-    
-    //file read and write end
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *path = [documentsDirectory stringByAppendingPathComponent:@"logfile.txt"];
+    // create if needed
+    if (![[NSFileManager defaultManager] fileExistsAtPath:path]){
+        fprintf(stderr,"Creating file at %s",[path UTF8String]);
+        [[NSData data] writeToFile:path atomically:YES];
+    }
+    // append
+    NSFileHandle *handle = [NSFileHandle fileHandleForWritingAtPath:path];
+    [handle truncateFileAtOffset:[handle seekToEndOfFile]];
+    [handle writeData:[string dataUsingEncoding:NSUTF8StringEncoding]];
+    [handle closeFile];
 }
 - (NSDictionary *)netStatsForInterval:(NSTimeInterval)sampleInterval {
     // Get sizing info from sysctl and resize as needed.
